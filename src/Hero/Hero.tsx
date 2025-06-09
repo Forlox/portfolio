46a486d22dbac0cca9ba2b0725314e2fcfd75bc9
Forlox <chugainov09032004@yandex.ru>
2025-06-10 01:06:07 +0500

    Попытки в анимации

--- src/Hero/Hero.tsx
+++ src/Hero/Hero.tsx
@@ -1,21 +1,29 @@
 import React, { JSX } from 'react';
+import ScrollAnimation from 'react-animate-on-scroll';
+import "animate.css/animate.compat.css";
 import logo from '../Assets/logo.svg';
 import './style.css';
 
 export function Hero(): JSX.Element {
     return (
-        <div className="hero-container">
-            <section id="home" className="hero-section">
-                <div className="hero-content">
-                    <h1 className="hero-heading">Привет, я Иван Иванов</h1>
-                    <p className="hero-text">
-                        Frontend разработчик с фокусом на создании чистых, функциональных и эстетичных пользовательских интерфейсов
-                    </p>
-                </div>
-                <div className="hero-image">
-                    <img src={logo} alt="Логотип" />
-                </div>
-            </section>
-        </div>
+        <ScrollAnimation animateIn="fadeIn" delay={1 * 100}>
+            <div className="hero-container">
+                <section id="home" className="hero-section">
+                    <div className="hero-content">
+                        <ScrollAnimation animateIn="fadeInUp" delay={2 * 100}>
+                            <h1 className="hero-heading">Привет, я Иван Иванов</h1>
+                        </ScrollAnimation>
+                        <ScrollAnimation animateIn="fadeInUp" delay={3 * 100}>
+                            <p className="hero-text">
+                                Frontend разработчик с фокусом на создании чистых, функциональных и эстетичных пользовательских интерфейсов
+                            </p>
+                        </ScrollAnimation>
+                    </div>
+                    <div className="hero-image">
+                        <img src={logo} alt="Логотип" />
+                    </div>
+                </section>
+            </div>
+        </ScrollAnimation>
     );
 }
